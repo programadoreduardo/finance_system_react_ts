bf2b00a4d909e5abea49c306b680866d9b2549fb
Eduardo Marques <programdor.eduardo@gmail.com>
2022-11-17 07:17:33 -0300

    exclusion of standard items

--- src/data/categories.ts
+++ src/data/categories.ts
@@ -2,7 +2,9 @@ import { Category } from "./types/Category"
 
 export const categories: Category = {
     food: { title: 'Alimentação', color: 'blue', expense: true },
+    several: { title: 'Diversos', color: 'red', expense: true },
     rent: { title: 'Aluguel', color: 'brown', expense: true },
-    salary: { title: 'Salário', color: 'green', expense: false }
+    salary: { title: 'Salário', color: 'green', expense: false },
+    yield: { title: 'Rendimentos', color: 'green', expense: false },
 }
 
--- src/data/items.ts
+++ src/data/items.ts
@@ -1,8 +1,4 @@
 import { Item } from "./types/Item"
 
 export const items: Item[] = [
-    { date: new Date (2022, 10, 6), category: 'food', title: 'McDonalds', value: 32.12 },
-    { date: new Date (2022, 10, 15), category: 'food', title: 'Burger King', value: 28 },
-    { date: new Date (2022, 10, 16), category: 'rent', title: 'Aluguel', value: 2300 },
-    { date: new Date (2022, 10, 18), category: 'salary', title: 'Sálario Acme', value: 4500 },
-];
+ ];
